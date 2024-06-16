c5d66ca9f1c31220a5fdba506b6f56643a3e7c02
Mohamed Kallel <medkallelmed@gmail.com>
2024-06-16 11:37:53 +0100

    backup

--- Src/Streamlit/pages/4_📦_Product_Data.py
+++ Src/Streamlit/pages/4_📦_Product_Data.py
@@ -3,3 +3,15 @@ from config import *
 import streamlit as st
 page_init()
 st.title("📦 Product Data")
+
+# Load the product data
+df_products = pd.read_csv(CLEAN_DATA_PATH + "product_cleaned.csv")
+st.write(
+    f"##### The dataset contains the product data of {df_products['PRODUCT_ID'].nunique():,} unique products sold by the retailer"
+)
+st.divider()
+
+st.write("- ##### General Product Statistics: ")
+st.write("###### We explore several statistics around the product data")
+st.write(df_products.describe(include="all"))
+st.dataframe(df_products.head())
